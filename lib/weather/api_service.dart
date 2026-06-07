daed001208de24ddc5d9f9f15bd6f11456ed3726
Prabesh Bhattarai <prabesh605@gmail.com>
2026-06-07 09:25:18 +0545

    weather app

--- lib/weather/api_service.dart
+++ lib/weather/api_service.dart
@@ -1,21 +1,45 @@
 import 'dart:convert';
 
 import 'package:broadway_bmi_cal/weather/weather_model.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
+  String apiKey = "0b10461d5a30e6612ce19d84c97ef651";
+
   Future<WeatherModel> getWeatherData(double lat, double lon) async {
-    String apiKey = "0b10461d5a30e6612ce19d84c97ef651";
-    var response = await http.get(
-      Uri.parse(
-        "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey",
-      ),
-    );
+    String url =
+        "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey";
+
+    var response = await http.get(Uri.parse(url));
     var json = jsonDecode(response.body);
-    //
     WeatherModel weather = WeatherModel.fromJson(json);
-  
-    //
     return weather;
+  }
+
+  Future<WeatherModel> getWeatherByCountry(String country) async {
+    String url =
+        "https://api.openweathermap.org/data/2.5/weather?q=$country&appid=$apiKey";
+    var response = await http.get(Uri.parse(url));
+    var json = jsonDecode(response.body);
+    WeatherModel weather = WeatherModel.fromJson(json);
+    return weather;
+  }
+
+  Future<Position> getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error("Location Not enabled");
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error("Location permission denied");
+      }
+    }
+    return await Geolocator.getCurrentPosition();
   }
 }
--- lib/weather/weather_screen.dart
+++ lib/weather/weather_screen.dart
@@ -11,25 +11,8 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
-  String apiKey = '0b10461d5a30e6612ce19d84c97ef651';
   ApiService apiService = ApiService();
-  //
-  Future<Position> getLocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      return Future.error("Location Not enabled");
-    }
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error("Location permission denied");
-      }
-    }
-    return await Geolocator.getCurrentPosition();
-  }
+  TextEditingController controller = TextEditingController();
 
   WeatherModel? data;
   double? temp;
@@ -40,57 +23,145 @@ class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Weather Screen")),
+      backgroundColor: Color(0xff080b30),
+      appBar: AppBar(
+        backgroundColor: Color(0xff080b30),
+        title: Text("Weather Screen", style: TextStyle(color: Colors.white)),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          Position position = await getLocation();
-          //step 1 get locaion
-          // step 2 we get lat and lon from current location
-
-          print(position);
+          Position position = await apiService.getCurrentLocation();
           double lat = position.latitude;
           double lon = position.longitude;
           //
-
           data = await apiService.getWeatherData(lat, lon);
           setState(() {});
-
-          // if (data != null) {
-          //   temp = data!.temp;
-          //   name = data!.name;
-          //   speed = data!.speed;
-          // }
-
-          // print(data['coord']);
         },
         child: Icon(Icons.my_location),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // data != null ? Text('${data["coord"]}') : Container(),
-            Text(
-              "Temperature is ${data?.main.temp ?? 0.0}",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
-            ),
-            Text(
-              "Sunrise = ${data?.sys.sunrise ?? ''}",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
-            ),
-            Text(
-              "Speed is ${speed ?? 0.0}",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
-            ),
-            // ElevatedButton(
-            //   onPressed: () async {
-            //     // dynamic data = await apiService.getWeatherData();
-            //     // print(data['coord']);
-            //   },
-            //   child: Text("Location"),
-            // ),
-          ],
+        child: Container(
+          margin: EdgeInsets.all(12),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      style: TextStyle(color: Colors.white),
+                      controller: controller,
+                      decoration: InputDecoration(border: OutlineInputBorder()),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  IconButton(
+                    onPressed: () async {
+                      String country = controller.text;
+                      data = await apiService.getWeatherByCountry(country);
+                      setState(() {});
+                    },
+                    icon: Icon(Icons.search, color: Colors.white),
+                  ),
+                ],
+              ),
+              // data != null ? Text('${data["coord"]}') : Container(),
+              Text("☁︎", style: TextStyle(color: Colors.white, fontSize: 180)),
+
+              Text("Wednesday 25 Dec", style: TextStyle(color: Colors.white)),
+              SizedBox(height: 20),
+              Text(
+                "${data?.main.temp ?? 0.0}°",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 26,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 120),
+              Row(
+                children: [
+                  Expanded(
+                    child: CutomWidget(name: 'Rain', value: '90%', image: '☁︎'),
+                  ),
+                  Expanded(
+                    child: CutomWidget(
+                      name: 'Wind',
+                      value: '7 km/h',
+                      image: '☁︎',
+                    ),
+                  ),
+
+                  Expanded(
+                    child: CutomWidget(
+                      name: 'Humidity',
+                      value: '${data?.main.humidity ?? 0.0}%',
+                      image: '☁︎',
+                    ),
+                  ),
+                ],
+              ),
+
+              // Text(
+              //   "Sunrise = ${data?.sys.sunrise ?? ''}",
+              //   style: TextStyle(
+              //     fontWeight: FontWeight.bold,
+              //     fontSize: 26,
+              //     color: Colors.white,
+              //   ),
+              // ),
+              // Text(
+              //   "Speed is ${speed ?? 0.0}",
+              //   style: TextStyle(
+              //     fontWeight: FontWeight.bold,
+              //     fontSize: 26,
+              //     color: Colors.white,
+              //   ),
+              // ),
+              // Text("${data?.name}"),
+              // Text("${data?.sys.country}"),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class CutomWidget extends StatelessWidget {
+  const CutomWidget({
+    super.key,
+    required this.name,
+    required this.value,
+    required this.image,
+  });
+  final String name;
+  final String value;
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(5),
+      padding: EdgeInsets.symmetric(vertical: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Color(0xff1b1e48),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(image, style: TextStyle(color: Colors.white, fontSize: 50)),
+          Text(value, style: TextStyle(color: Colors.white, fontSize: 22)),
+          Text(
+            name,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
       ),
     );
   }
